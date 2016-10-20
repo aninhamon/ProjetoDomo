3c611a3f87b8d8dc031c62e171005fbc231e4fba
aninhamon <projetodomo@gmail.com>
2016-10-20 11:29:43 -0200

    Movimento do player ok, nova arma e cores

---
+++ Assets/Done_Scripts/CamRotate.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class CamRotate : MonoBehaviour
+{
+    public float rotateSpeed = 1f, scrollSpeed = 200f;
+    public Transform pivot;
+
+    public SphericalCoordinates sc;
+
+    private void Start()
+    {
+        sc = new SphericalCoordinates(transform.position, 3f, 10f, 0f, Mathf.PI * 2f, 0f, Mathf.PI / 4f);
+        // Initialize position
+        transform.position = sc.toCartesian + pivot.position;
+    }
+
+    void Update()
+    {
+        float kh, kv, mh, mv, h, v;
+        kh = Input.GetAxis("Horizontal");
+        kv = Input.GetAxis("Vertical");
+
+        bool anyMouseButton = Input.GetMouseButton(0) | Input.GetMouseButton(1) | Input.GetMouseButton(2);
+        mh = anyMouseButton ? Input.GetAxis("Mouse X") : 0f;
+        mv = anyMouseButton ? Input.GetAxis("Mouse Y") : 0f;
+
+        h = kh * kh > mh * mh ? kh : mh;
+        v = kv * kv > mv * mv ? kv : mv;
+
+        if (h * h > .1f || v * v > .1f)
+            transform.position = sc.Rotate(h * rotateSpeed * Time.deltaTime, v * rotateSpeed * Time.deltaTime).toCartesian + pivot.position;
+
+        float sw = -Input.GetAxis("Mouse ScrollWheel");
+        if (sw * sw > Mathf.Epsilon)
+            transform.position = sc.TranslateRadius(sw * Time.deltaTime * scrollSpeed).toCartesian + pivot.position;
+
+        transform.LookAt(pivot.position);
+    }
+}
--- Assets/Done_Scripts/Done_PlayerController.cs
+++ Assets/Done_Scripts/Done_PlayerController.cs
@@ -9,12 +9,85 @@ public class Done_Boundary
 
 public class Done_PlayerController : MonoBehaviour
 {
-	public float speed;
+    public string horizontalButton = "Horizontal_P1";
+    public string verticalButton = "Vertical_P1";
+
+    public string fireButton = "Fire_P1";
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    private float nextFire;
+
+    public float rotateSpeed = 1f, scrollSpeed = 200f;
+    public Transform pivot;
+
+    public SphericalCoordinates sc;
+
+    private void Start()
+    {
+        sc = new SphericalCoordinates(transform.position, 3f, 4.3f, 0f, Mathf.PI * 2f, 0f, Mathf.PI / 4f);
+        // Initialize position
+        transform.position = sc.toCartesian + pivot.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetButton(fireButton) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            GetComponent<AudioSource>().Play();
+            Destroy(shot, 0.5f);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float kh, kv, mh, mv, h;
+        kh = Input.GetAxis(horizontalButton);
+        kv = Input.GetAxis(verticalButton);
+
+        bool anyMouseButton = Input.GetMouseButton(0) | Input.GetMouseButton(1) | Input.GetMouseButton(2);
+        mh = anyMouseButton ? Input.GetAxis("Mouse X") : 0f;
+        mv = anyMouseButton ? Input.GetAxis("Mouse Y") : 0f;
+
+        h = 0;
+
+        if (Input.GetAxis(horizontalButton) != 0)
+        {
+            h = kh * kh > mh * mh ? kh : mh;            
+        }
+
+        if (Input.GetAxis(verticalButton) != 0)
+        {
+            if(Input.GetAxis(verticalButton) > 0)
+            {
+                if (sc.radius > 3f)
+                {
+                    sc.SetRadius(sc.radius - 0.1f);
+                }
+            }else
+            {
+                if (sc.radius < 4.3f)
+                {
+                    sc.SetRadius(sc.radius + 0.1f);
+                }
+            }
+        }
+                
+        transform.position = sc.Rotate(h * rotateSpeed * Time.deltaTime, 0).toCartesian + pivot.position;
+
+        transform.LookAt(pivot.position);
+
+    }
+
+
+
+    /*public float speed;
 	public float tilt;
 	public Done_Boundary boundary;
     public string fireButton = "Fire_P1";
-    public string horizontalButton = "Horizontal_P1";
-    public string verticalButton = "Vertical_P1";
+    
 
     public GameObject shot;
 	public Transform shotSpawn;
@@ -22,24 +95,17 @@ public class Done_PlayerController : MonoBehaviour
 	 
 	private float nextFire;
 
-    Vector3 CartesianToPolar(Vector3 point)
+    //NOVO
+    public float rotateSpeed = 1f, scrollSpeed = 200f;
+    public Transform pivot;
+
+    public SphericalCoordinates sc;
+
+    private void Start()
     {
-        Vector3 polar;
- 
-        //calc longitude
-        polar.z = Mathf.Atan2(point.x,point.z);
-
-        //this is easier to write and read than sqrt(pow(x,2), pow(y,2))!
-        float xzLen = Mathf.Sqrt(Mathf.Pow(point.x, 2) + Mathf.Pow(point.y, 2)); //Vector2(point.x, point.z).magnitude;
-        //atan2 does the magic
-        polar.x = Mathf.Atan2(-point.x,xzLen);
-
-        polar.y = 0.0f;
- 
-        //convert to deg
-        polar *= Mathf.Rad2Deg;
- 
-         return polar;
+        sc = new SphericalCoordinates(transform.position, 3f, 10f, 0f, Mathf.PI * 2f, 0f, Mathf.PI / 4f);
+        // Initialize position
+        transform.position = sc.toCartesian + pivot.position;
     }
 
     void Update ()
@@ -54,7 +120,7 @@ public class Done_PlayerController : MonoBehaviour
 
 	void FixedUpdate ()
 	{
-		float moveHorizontal = Input.GetAxis (horizontalButton);
+        /*float moveHorizontal = Input.GetAxis (horizontalButton);
 		float moveVertical = Input.GetAxis (verticalButton);
 
         Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
@@ -71,5 +137,6 @@ public class Done_PlayerController : MonoBehaviour
 		);
 		
 		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
-	}
+        }*/
+
 }
